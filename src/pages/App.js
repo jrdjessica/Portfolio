7129d159c27f613ec50d16bce39583a9da161541
Jessica Dea <jessicardea@gmail.com>
2022-12-19 22:50:12 -0800

    Changed homepage path

--- src/pages/App.js
+++ src/pages/App.js
@@ -15,7 +15,7 @@ function App() {
     <div>
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/portfolio" element={<Home />} />
           <Route path="/skills" element={<Skills />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
