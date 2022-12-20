e05a462fd093b7edc35392262764cf7f6a96c499
Jessica Dea <jessicardea@gmail.com>
2022-12-20 00:26:42 -0800

    Updates

---
+++ precache-manifest.95859933689184415981d5ab9a059db1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0320ba3fb0587ee4e69d",
+    "url": "/portfolio/static/css/main.214cbfc5.chunk.css"
+  },
+  {
+    "revision": "0320ba3fb0587ee4e69d",
+    "url": "/portfolio/static/js/main.0320ba3f.chunk.js"
+  },
+  {
+    "revision": "1b3f48c6de62d66542ce",
+    "url": "/portfolio/static/js/1.1b3f48c6.chunk.js"
+  },
+  {
+    "revision": "83f3b8b2322a5a5da28f",
+    "url": "/portfolio/static/js/2.83f3b8b2.chunk.js"
+  },
+  {
+    "revision": "195ec0513de6420f5340",
+    "url": "/portfolio/static/js/runtime~main.195ec051.js"
+  },
+  {
+    "revision": "fca49251111964bf8b30325df8f1eec0",
+    "url": "/portfolio/index.html"
+  }
+];
